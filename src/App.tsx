b5038a8a451884840ffb279a45d54f56d0ec5539
MilosMilovan <milos.milovanovic@bwt.ee>
2023-05-27 17:12:49 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { useMemo } from "react"
+import { Container } from "react-bootstrap"
+import { Routes, Route, Navigate } from "react-router-dom"
+
+function App() {
+  return (
+    <Container className="my-4">
+      <h1>Home</h1>
+    </Container>
+  )
+}
+
+export default App
