ce7adb554823dd6ec2f3c3522dea4ed9475d34eb
MilosLogispin <m.milovanovic@logispin.com>
2023-05-29 16:50:53 +0200

    added note new

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { useMemo } from "react"
 import { Container } from "react-bootstrap"
 import { Routes, Route, Navigate } from "react-router-dom"
+import Home from "./components/Home"
+import NewNote from "./components/NewNote"
+import EditNote from "./components/EditNote"
+import Note from "./components/Note"
 
 function App() {
   return (
     <Container className="my-4">
-      <h1>Home</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id" >
+          <Route index element={<Note />} />
+          <Route path="edit" element={<EditNote />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </Container>
   )
 }
